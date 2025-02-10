d5942968bad7b6800296273f9a337fcea9e03035
Dominick Smith <dominick.smith938@gmail.com>
2025-02-10 10:58:47 -0600

    resume route and pdf parsing fixed

--- pages/resume.jsx
+++ pages/resume.jsx
@@ -2,20 +2,23 @@ import { Document, Page, pdfjs } from 'react-pdf';
 import { useState, useEffect } from 'react';
 import React from 'react';
 import styles from '../components/resume.module.css';
+import Navbar from '../components/navbar.jsx';
+
 
 function Resume() {
- useEffect(() => {
-    pdfjs.GlobalWorkerOptions.workerSrc = "/pdfjs.worker.min.mjs";
-  }, []); 
+   useEffect(() => {
+    pdfjs.GlobalWorkerOptions.workerSrc = "/pdf.worker.min.mjs";
+  }, []);
   const [numPages, setNumPages] = useState(null);
-    
+ 
   function onLoadSuccess({ numPages }) {
     setNumPages(numPages);
   }
 
   return (
-      <div id={styles.pdfdoc}>
-      <Document file='../public/resume.pdf' onLoadSuccess={onLoadSuccess}>
+      <div className={styles.pdfdoc}>
+      <Navbar />
+      <Document file='./resume.pdf' onLoadSuccess={onLoadSuccess}>
         {[...Array(numPages)].map((_, index) => (
           <Page key={index} pageNumber={index + 1} scale={1.8} renderMode="canvas" renderTextLayer={false} renderAnnotationLayer={false}/>
         ))}
